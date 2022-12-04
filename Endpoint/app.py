69242b259f62c74077c40c8fad4512d0fbcc2023
shriram96 <shriramravi96@gmail.com>
2022-12-04 02:23:16 +0000

    ML Model

--- Endpoint/app.py
+++ Endpoint/app.py
@@ -5,6 +5,13 @@ import json
 import requests
 import logging
 
+import torch
+import json
+import numpy as np
+from transformers import BertTokenizer
+from torch import nn
+from transformers import BertModel
+
 logger = logging.getLogger()
 
 app = Flask(__name__)
@@ -12,11 +19,29 @@ app = Flask(__name__)
 IP = "localhost"
 url = "http://34.124.115.77:8983/solr/chitchat/query?fl=text, reply, score, topic"
 
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
 
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
-
 
 @app.route('/send', methods=['POST'])
 def send_message():
@@ -30,6 +55,10 @@ def send_message():
         list_topics: list = ["topic:" + topic for topic in topics.split(',')]
         filters = ' OR '.join(list_topics)
 
+    # print(find_type(text))
+    if(find_type(text) == 'chitchat'):
+        filters = "topic:chitchat"
+    
     payload = json.dumps({
         "query": "text: " + text,
         "limit": 20,
@@ -47,6 +76,23 @@ def send_message():
 
     return ret
 
+def find_type(text: str):
+    labels = {
+        'reddit':0,
+        'chitchat':1
+    }
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    texts = tokenizer(text, 
+                    padding='max_length', max_length = 512, truncation=True,
+                    return_tensors="pt")
+    mask = texts['attention_mask'].to(device)
+    input_id = texts['input_ids'].squeeze(1).to(device)
+
+    output = model(input_id, mask)
+    # print('Query:', text)
+    # print(list(labels.keys())[(torch.argmax(output).item())])
+    return list(labels.keys())[(torch.argmax(output).item())]
 
 def get_processed_message(response: dict) -> dict:
     docs = response.get("response").get("docs")
@@ -59,4 +105,8 @@ def get_processed_message(response: dict) -> dict:
 
 
 if __name__ == "__main__":
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+    model = BertClassifier()
+    model.load_state_dict(torch.load('../ML/binary_bert_model.pt',map_location=torch.device('cpu')))
+    model.eval()
     app.run(host=IP, port=9999, debug=True)
